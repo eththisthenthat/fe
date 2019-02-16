91b8c9224d84b96a781fbc0f5f7fa613e3f83c6e
John D. Storey <johndangerstorey@gmail.com>
2019-02-16 10:23:10 -0700

    adds blocknative

--- src/index.js
+++ src/index.js
@@ -2,7 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import assist from 'bnc-assist'
 import * as serviceWorker from './serviceWorker';
+var bncAssistConfig = {
+  dappId: '26fb439f-ff1e-48f8-9ddf-29a6dcf7fd49',       // [String] The API key supplied to you by Blocknative when you sign up for an account
+
+  networkId:  4 // [Integer] The network ID of the Ethereum network your dapp is deployd on.
+                        //           See below for instructions on how to setup for local blockchains.
+};
+
+var assistInstance = assist.init(bncAssistConfig);
+assistInstance.onboard()
+  .then(function(success) {
+    console.log('success', success)
+    // User has been successfully onboarded and is ready to transact
+    // This means we can be sure of the follwing user properties:
+    //  - They are using a compatible browser
+    //  - They have a web3-enabled wallet installed
+    //  - The wallet is connected to the config-specified networkId
+    //  - The wallet is unlocked and contains at least `minimumBalance` in wei
+    //  - They have connected their wallet to the dapp, congruent with EIP1102
+  })
+  .catch(function(error) {
+    // The user exited onboarding before completion
+    // Will let you know what stage of onboarding the user was up to when they exited
+    console.log(error.msg);
+  })
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
