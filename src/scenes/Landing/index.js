3e72658d05b34e1911312415cfaf40c60a14aa32
Corbin Page <corbin.page@gmail.com>
2019-02-16 14:43:55 -0700

    Added coming soon cards

--- src/scenes/Landing/index.js
+++ src/scenes/Landing/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Container } from '../../components'
 import TriggerEthPriceDropCard from '../../components/Card/Cards/triggerEthPriceDropCard'
-import ActionTransfer from '../../components/Card/Cards/actionTransfer'
+import ActionTransferCard from '../../components/Card/Cards/actionTransferCard'
 
 export default () => {
   return <Container>
   	<h2>My Tasks</h2>
   	<TriggerEthPriceDropCard isDisplay={true} price={100} />
   	<TriggerEthPriceDropCard isDisplay={false} price={100} />
-  	<ActionTransfer isDisplay={true} />
-  	<ActionTransfer isDisplay={false} />
+  	<ActionTransferCard isDisplay={true} />
+  	<ActionTransferCard isDisplay={false} />
   </Container>
 }
