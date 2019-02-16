f812e59ea5c2901675cea53e70e18166a6ad6b85
John D. Storey <johndangerstorey@gmail.com>
2019-02-16 14:42:18 -0700

    stops errors

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
@@ -25,7 +26,6 @@ function SimpleCard(props) {
   return (
     <StyledCard 
       className={`bg-${color} mb${10} relative overflow-visible`} 
-      classes=''
       data-trigger={isTrigger}>
       <CardContent>
         <Typography className={`txt-white`} gutterBottom>
@@ -45,4 +45,4 @@ SimpleCard.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default SimpleCard;
+export default withStyles({})(SimpleCard);
