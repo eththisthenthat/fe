e9a8e6227ec8241a3784b3782fb9106fe48204e5
John D. Storey <johndangerstorey@gmail.com>
2019-02-17 11:37:47 -0700

    cleanup

--- src/utils/getApiUrl.js
+++ src/utils/getApiUrl.js
@@ -1,6 +1,5 @@
 export default () => {
   const customEndpoint = process.env.REACT_APP_CUSTOM_API
-  console.log('customEndpoint: ', customEndpoint, process.env);
   return customEndpoint 
     ? customEndpoint 
     : 'https://d0ob9xv927.execute-api.us-east-1.amazonaws.com/dev'
