1b457f5b0b804df8492adb9ed21e4eaf37667209
Corbin Page <corbin.page@gmail.com>
2019-02-16 23:40:04 -0700

    GOt task history to work

--- src/components/TaskHistory/index.js
+++ src/components/TaskHistory/index.js
@@ -1,20 +1,17 @@
 import React from 'react'
+import moment from 'moment'
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import Link from '@material-ui/core/Link';
+import shorten from '../../utils/shortenAddress'
 
-function TaskHistory(props) {
+function TaskHistory({task, ...props}) {
 
   // Get Task History for Task
-  const taskHistory = {
-  	data: [
-  		{id: 1, status: 'Success', time: '2019-2-17 10:02 AM'},
-  		{id: 2, status: 'Success', time: '2019-2-17 7:37 AM'},
-  		{id: 3, status: 'Success', time: '2019-2-17 7:30 AM'}
-  	]
-  }
+  const taskHistory = task.taskHistory;
 
   return (
     <div style={{width: '100%'}}>
@@ -24,13 +21,19 @@ function TaskHistory(props) {
           <TableRow>
             <TableCell>Time</TableCell>
             <TableCell>Status</TableCell>
+            <TableCell>Transaction</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {taskHistory.data.map(row => (
-            <TableRow key={row.id}>
-              <TableCell>{row.time}</TableCell>
+          {taskHistory.map((row,i) => (
+            <TableRow key={i}>
+              <TableCell>{moment(parseInt(row.startTime)).format('MMM Do YYYY H:mm')}</TableCell>
               <TableCell>{row.status}</TableCell>
+              <TableCell>
+                <Link href={`https://rinkeby.etherscan.io/tx/${row.txHash}`} target="_blank">
+                  { shorten(row.txHash) }
+                </Link>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
