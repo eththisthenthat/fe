7b9dddf546add972fcdc0f7112e71be097e0ab62
John D. Storey <johndangerstorey@gmail.com>
2019-02-16 20:22:53 -0700

    changes active on click

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -10,6 +10,7 @@ import ActionTransferCard from '../Card/Cards/actionTransferCard'
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import MuiExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import axios from 'axios'
 import styled from 'styled-components'
 
 function renderTriggerCard(task) {
@@ -63,6 +64,14 @@ function Task(props) {
   const { isDisplay, task, isActive } = props;
   const [expanded, setExpanded] = useState(false);
   const [active, setActive] = useState(isActive);
+  const handleChange = async (e) => {
+    setActive(e.target.value)
+    const res = await axios.put(`https://it3ptht0ig.execute-api.us-east-1.amazonaws.com/dev/tasks/${task.id}`,{
+      active: e.target.value
+    })
+    console.log(res)
+    // handle api call
+  }
 
   return (
   	<div className={`flex mb10`}>
@@ -77,9 +86,8 @@ function Task(props) {
       </ExpansionPanel>
       <StyledFormGroup>
   			<FormControlLabel
-          control={
-            <Checkbox color="default" value="checkedG" />
-          }
+          control={ <Checkbox color="default" checked={active} /> }
+          onChange={handleChange}
         	label="Active"
       	/>
       </StyledFormGroup>
